e02abd0c9b6101bdfd23b66b092c5bef3bca0859
Key <springfield0329@gmail.com>
2022-08-08 03:04:38 +0800

    modal v1

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
         $(".editor").toggleClass("d-none");
         $(".card-show").toggleClass("d-none");
     });
+
+    $(".expand-more").click(function (e) { 
+        event.preventDefault();
+        // console.log("click");
+        $(".button-show").removeClass("d-none");
+        $(".expand-less").removeClass("d-none");
+        $(".expand-more").addClass("d-none");
+    });
+    $(".expand-less").click(function (e) { 
+        event.preventDefault();
+        // console.log("click");
+        $(".button-show").addClass("d-none");
+        $(".expand-less").addClass("d-none");
+        $(".expand-more").removeClass("d-none");
+    });
+
 });
+
