43ae23183484271931d397cee74438416ec4d789
Key <springfield0329@gmail.com>
2022-08-07 18:21:50 +0800

    assignment finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,14 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+    $(".reply").click(function (e) { 
+        event.preventDefault();
+        // console.log("click");
+        $(".editor").toggleClass("d-none");
+        $(".card-show").toggleClass("d-none");
+    });
+    $(".cancel").click(function (e) { 
+        event.preventDefault();
+        // console.log("click");
+        $(".editor").toggleClass("d-none");
+        $(".card-show").toggleClass("d-none");
+    });
 });
