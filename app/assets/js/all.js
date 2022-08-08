102434da3afa90d53baa4059e2928ecddb1beb14
Key <springfield0329@gmail.com>
2022-08-08 13:25:50 +0800

    admin finish & modal v2

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,30 +1,20 @@
 $(document).ready(function () {
     $(".reply").click(function (e) { 
         event.preventDefault();
-        // console.log("click");
-        $(".editor").toggleClass("d-none");
-        $(".card-show").toggleClass("d-none");
+        $(".editor, .card-show").toggleClass("d-none");
     });
     $(".cancel").click(function (e) { 
         event.preventDefault();
-        // console.log("click");
-        $(".editor").toggleClass("d-none");
-        $(".card-show").toggleClass("d-none");
+        $(".editor, .card-show").toggleClass("d-none");
     });
 
     $(".expand-more").click(function (e) { 
         event.preventDefault();
-        // console.log("click");
-        $(".button-show").removeClass("d-none");
-        $(".expand-less").removeClass("d-none");
-        $(".expand-more").addClass("d-none");
+        $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
     });
     $(".expand-less").click(function (e) { 
         event.preventDefault();
-        // console.log("click");
-        $(".button-show").addClass("d-none");
-        $(".expand-less").addClass("d-none");
-        $(".expand-more").removeClass("d-none");
+        $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
     });
 
 });
