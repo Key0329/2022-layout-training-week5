52f55d9b3f4da01e691a94e239fa622bb4ba16b2
Key <springfield0329@gmail.com>
2022-08-10 22:01:11 +0800

    Update 2022-08-10T14:01:07.987Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,14 +8,13 @@ $(document).ready(function () {
   $(".cancel").click(function (e) {
     event.preventDefault();
     $(".editor, .card-show").toggleClass("d-none");
-  });
-  $(".expand-more").click(function (e) {
-    event.preventDefault();
-    $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
-  });
-  $(".expand-less").click(function (e) {
-    event.preventDefault();
-    $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
-  });
+  }); // $(".expand-more").click(function (e) { 
+  //     event.preventDefault();
+  //     $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
+  // });
+  // $(".expand-less").click(function (e) { 
+  //     event.preventDefault();
+  //     $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
+  // });
 });
 //# sourceMappingURL=all.js.map
