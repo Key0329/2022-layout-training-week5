3038638469f69cc9f30ea75bd7f2dd42f65da97b
Key <springfield0329@gmail.com>
2022-08-10 22:00:37 +0800

    advice fix

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,14 +8,14 @@ $(document).ready(function () {
         $(".editor, .card-show").toggleClass("d-none");
     });
 
-    $(".expand-more").click(function (e) { 
-        event.preventDefault();
-        $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
-    });
-    $(".expand-less").click(function (e) { 
-        event.preventDefault();
-        $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
-    });
+    // $(".expand-more").click(function (e) { 
+    //     event.preventDefault();
+    //     $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
+    // });
+    // $(".expand-less").click(function (e) { 
+    //     event.preventDefault();
+    //     $(".button-show, .expand-less, .expand-more").toggleClass("d-none");
+    // });
 
 });
 
